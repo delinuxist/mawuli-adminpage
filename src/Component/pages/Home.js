7be27667999dd81f067c940270e6d04cb49d022d
delinuxist <delinuxist@gmail.com>
2021-09-29 20:49:01 +0000

    home page

---
+++ src/Component/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import InputItems from "../Demo/inputItems";
+import InputUsers from "../Demo/InputUsers";
+
+export default function Home() {
+  return (
+    <div
+      className="home
+     "
+    >
+      <InputUsers />
+      <br />
+      <InputItems />
+    </div>
+  );
+}
